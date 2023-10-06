bacd7b6e6df68fd5dbd84b549a6150cf36449e19
hwcsaravena <hwc.saravena@gmail.com>
2023-10-06 17:51:46 -0500

    a ver

---
+++ script.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+
+});
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints : {
+        0: {
+            slidesPerView: 1
+        },
+        520: {
+            slidesPerView: 2
+        },
+        950: {
+            slidesPerView: 3
+        }
+    }
+
+});
+
+
+
